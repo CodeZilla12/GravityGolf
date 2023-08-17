arctan2 fixes dxdy sign flips in update_velocity

main.py
@@ -7,8 +7,8 @@ class Window:
         self.WINDOW_WIDTH = 800
         self.WINDOW_HEIGHT = 400
         
-        self.object_list = [[0,np.asarray([0,0]),np.asarray([50,50]), 10],
-                            [1,np.asarray([0,0]),np.asarray([750,350]), 10]
+        self.object_list = [[0,np.asarray([50,0],dtype = np.float64),np.asarray([200,300]), 10],
+                            [1,np.asarray([0,0],dtype = np.float64),np.asarray([400,200]), 10e10]
         ] #sublists give id int,velocity(vx,vy) numpy, position(x,y) numpy, mass(m) numlike
         
         pygame.init()
@@ -22,14 +22,13 @@ class Window:
     def main_loop(self):
         
         running = True
-        self.SCREEN.fill((0,0,0))
+        
         while running:
+            self.SCREEN.fill((0,0,0))
             
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
-            
-            
             
             for i,object in enumerate(self.object_list):
                 position = [object[2][0], self.WINDOW_HEIGHT - object[2][1]] #refactor this
@@ -47,7 +46,7 @@ class Window:
             self.CLOCK.tick(30)
     
     def update_position(self,object):
-        id, velocities, positions, _mass = object #refactor as dictionary
+        id, velocities, positions, _mass = object #refactor as dictionary or dataclass
         
         positions = positions + velocities * self.DELTA_T
         
@@ -61,27 +60,28 @@ class Window:
         ΔVy = GMsin(θ)/(r*t)
         """
         
-        delta_velocities = np.asarray([0,0])
+        delta_velocities = np.asarray([0,0], dtype = np.float64)
         delta_t = 1/self.FPS #change to real delta_t
         
         for other_object in self.object_list:
-            other_id,_,other_positions,other_mass = object
+            other_id,_,other_positions,other_mass = other_object
             if other_id == id:
                 continue
                 
             dx,dy = other_positions - positions
             
-            angle = np.arctan(dx/dy)
+            #angle = np.arctan(dx/dy)
+            angle = np.arctan2(dy,dx) #electric boogaloo how function
             separation = np.hypot(dx,dy)
             
             delta_velocities += np.asarray([
                 self.G * other_mass * np.cos(angle) / (separation * self.DELTA_T),
                 self.G * other_mass * np.sin(angle) / (separation * self.DELTA_T)
-            ]) #[delta_vx, delta_vy]
-            
-        velocities += np.asarray([10,10])
-        self.object_list[id] = [id,velocities,positions,_mass]
-        return
+            ], dtype = np.float64) #[delta_vx, delta_vy]
+
+        #velocities += np.asarray([10,10])
+        #self.object_list[id] = [id,velocities,positions,_mass]
+        #return
         
         velocities += delta_velocities
         