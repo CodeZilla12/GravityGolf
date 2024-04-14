1d3d10df37e8bcebf6793ecea9e43d2a1484b6d2
CodeZilla12 <CodeZilla12@github.com>
2024-04-14 20:38:35 +0100

    Reordered functions

--- main.py
+++ main.py
@@ -76,40 +76,7 @@ class Window:
         self.scenario = 1  # default scenario
         self.start_scenario()
 
-    def draw_scale_bar(self) -> None:
-        """_summary_
-        """
-
-        x1 = 50
-        x2 = x1 + (self.AU * self.AU_PIXELS_CONVERSION)
-
-        y = 50
-        half_arm_width = 25
-
-        pygame.draw.line(self.SCREEN, (255, 255, 255),
-                         (x1, self.WINDOW_HEIGHT-y), (x2, self.WINDOW_HEIGHT-y))
-        pygame.draw.line(self.SCREEN, (255, 255, 255), (x1, self.WINDOW_HEIGHT -
-                         y-half_arm_width), (x1, self.WINDOW_HEIGHT-y+half_arm_width))
-        pygame.draw.line(self.SCREEN, (255, 255, 255), (x2, self.WINDOW_HEIGHT -
-                         y-half_arm_width), (x2, self.WINDOW_HEIGHT-y+half_arm_width))
-        self.TEXT_FONT.render_to(self.SCREEN, ((
-            x2-x1)/2, self.WINDOW_HEIGHT-y-half_arm_width), '1 AU', (250, 250, 250))
-
-        self.seconds_passed += self.delta_t * self.time_mult
-
-        years_passed = self.seconds_passed / (3600*24*365)
-
-        months_passed = 12 * (years_passed % 1)
-
-        years_passed = self.seconds_passed / (3600*24*365)
-
-        self.TEXT_FONT.render_to(self.SCREEN, (
-            x2+50, self.WINDOW_HEIGHT-2*half_arm_width), f"{np.floor(years_passed)} Years {np.floor(months_passed)} Month(s) Passed", (250, 250, 250))
-        self.TEXT_FONT.render_to(self.SCREEN, (
-            x2+50, self.WINDOW_HEIGHT-4*half_arm_width), f"{self.number_of_shots_taken} Shots Taken", (250, 250, 250))
-
     def start_scenario(self):
-
         if self.sandbox_mode:
             self.show_notification("Sandbox mode Active")
             return
@@ -146,145 +113,8 @@ class Window:
                 (0, self.WINDOW_WIDTH/self.AU_PIXELS_CONVERSION), (0, self.WINDOW_HEIGHT/self.AU_PIXELS_CONVERSION)) for _ in range(100)]
 
             self.NOTIFICATION_FADEOUT = 1
-            self.show_notification(
-                f"You Won! Final Score: {round(self.final_score)}. Press R to Restart")
-
-    def calculate_slingshot_velocity(self) -> tuple:
-        # function name likely needs changing
-        # refers to the slingshot motion of spawning new planets
-        mouse_x, mouse_y = pygame.mouse.get_pos()
-        stored_x, stored_y = self.mouse_click_coordinate_pixels
-
-        if None in self.mouse_click_coordinate_pixels:
-            return None
-
-        dy = stored_y - mouse_y
-        dx = stored_x - mouse_x
-
-        au_x = stored_x / self.AU_PIXELS_CONVERSION
-        au_y = (self.WINDOW_HEIGHT - stored_y) / self.AU_PIXELS_CONVERSION
-
-        coordinates_au = [au_x, au_y]
-
-        vx = dx * self.slingshot_power
-        vy = - dy * self.slingshot_power
-
-        velocities = [vx, vy]
-
-        return au_x, au_y, vx, vy
-
-    def show_notification(self, text=None) -> None:
-        x = 10
-        y = 50
-
-        if text:
-            self.notification_text = text
-            self.notification_opacity = 1
-
-        color = np.array((250, 250, 250), dtype=np.int32) * \
-            self.notification_opacity
-        if any(color) < 0:
-            self.notification_opacity = 1
-            self.notification_text = ""
-
-        self.TEXT_FONT.render_to(
-            self.SCREEN, (x, y), self.notification_text, color)
-
-        self.notification_opacity *= self.NOTIFICATION_FADEOUT
-
-    def draw_arrow(self, start_pos, end_pos) -> None:
-
-        new_end_pos = (2*start_pos - end_pos)
-
-        pygame.draw.line(self.SCREEN, (255, 255, 255), start_pos, new_end_pos)
-
-        _, _, vx, vy = self.calculate_slingshot_velocity()
-
-        velocity = f"{round(np.hypot(vx, vy)*1e-3)}Km/s"
-
-        self.TEXT_FONT.render_to(
-            self.SCREEN, new_end_pos + 10, str(velocity), (250, 250, 250))
-
-    def mouse_event_handler(self, event):
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            left_mouse_pressed = pygame.mouse.get_pressed()[0]
-
-            if left_mouse_pressed:
-
-                mouse_x, mouse_y = pygame.mouse.get_pos()
-
-                self.mouse_click_coordinate_pixels = [mouse_x, mouse_y]
-
-        elif event.type == pygame.MOUSEBUTTONUP:
-            left_mouse_released = not pygame.mouse.get_pressed()[0]
-
-            if left_mouse_released:
-
-                au_vel = self.calculate_slingshot_velocity()
-
-                # if no stored coordinates
-                if au_vel is None:
-                    return
-
-                au_x, au_y, vx, vy = au_vel
-
-                planet = self.planet_list[self.selected_planet]
-
-                self.object_list.append(
-                    PointMass([vx, vy], [au_x,
-                                         au_y], planet.mass, radius=7e9, colour=planet.colour, player_spawned=True)
-                )
-
-                self.mouse_click_coordinate_pixels = [None, None]
-                self.number_of_shots_taken += 1
-
-    def keyboard_event_handler(self, event):
-        if event.type == pygame.KEYDOWN:
-
-            # If escape is pressed when lmb is held down
-            if event.key == pygame.K_ESCAPE and pygame.mouse.get_pressed()[0] == True:
-                self.mouse_click_coordinate_pixels = [
-                    None, None]  # Cancel action
-                self.show_notification("Input Cancelled")
-
-            elif event.key == pygame.K_COMMA:
-                self.time_mult /= 5
-                self.show_notification(f"x{self.time_mult:.0f} Speed")
-
-            elif event.key == pygame.K_PERIOD:
-                self.time_mult *= 5
-                self.show_notification(f"x{self.time_mult:.0f} Speed")
-
-            elif event.unicode.isdigit():
-                self.selected_planet = int(event.unicode)
-                planet = self.planet_list[self.selected_planet]
-                self.show_notification(
-                    f"{planet.name}: {planet.mass:.0e}Kg")
-
-            elif event.key == pygame.K_r:
-                # restart game
-                self.seconds_passed = 0
-                self.notification_opacity = 0.98
-                self.max_velocity = 0
-                self.number_of_shots_taken = 0
-                self.scenario = 1
-                self.start_scenario()
-
-    def event_handler(self) -> bool:
-
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                return False
-
-            self.mouse_event_handler(event)
-
-            self.keyboard_event_handler(event)
-
-        if not None in self.mouse_click_coordinate_pixels:
-            self.draw_arrow(np.array(self.mouse_click_coordinate_pixels), np.array(
-                pygame.mouse.get_pos()))
-
-        return True
+            # self.show_notification(
+            #    f"You Won! Final Score: {round(self.final_score)}. Press R to Restart")
 
     def main_loop(self) -> None:
         """_summary_
@@ -408,6 +238,175 @@ class Window:
         # Updates positions with *Net* velocities, hence why outside the loop. Reduces number of calculations per frame.
         object.positions = object.positions + \
             (object.velocities * self.delta_t * self.time_mult)
+
+    def calculate_slingshot_velocity(self) -> tuple:
+        # function name likely needs changing
+        # refers to the slingshot motion of spawning new planets
+        mouse_x, mouse_y = pygame.mouse.get_pos()
+        stored_x, stored_y = self.mouse_click_coordinate_pixels
+
+        if None in self.mouse_click_coordinate_pixels:
+            return None
+
+        dy = stored_y - mouse_y
+        dx = stored_x - mouse_x
+
+        au_x = stored_x / self.AU_PIXELS_CONVERSION
+        au_y = (self.WINDOW_HEIGHT - stored_y) / self.AU_PIXELS_CONVERSION
+
+        coordinates_au = [au_x, au_y]
+
+        vx = dx * self.slingshot_power
+        vy = - dy * self.slingshot_power
+
+        velocities = [vx, vy]
+
+        return au_x, au_y, vx, vy
+
+    def mouse_event_handler(self, event):
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            left_mouse_pressed = pygame.mouse.get_pressed()[0]
+
+            if left_mouse_pressed:
+
+                mouse_x, mouse_y = pygame.mouse.get_pos()
+
+                self.mouse_click_coordinate_pixels = [mouse_x, mouse_y]
+
+        elif event.type == pygame.MOUSEBUTTONUP:
+            left_mouse_released = not pygame.mouse.get_pressed()[0]
+
+            if left_mouse_released:
+
+                au_vel = self.calculate_slingshot_velocity()
+
+                # if no stored coordinates
+                if au_vel is None:
+                    return
+
+                au_x, au_y, vx, vy = au_vel
+
+                planet = self.planet_list[self.selected_planet]
+
+                self.object_list.append(
+                    PointMass([vx, vy], [au_x,
+                                         au_y], planet.mass, radius=7e9, colour=planet.colour, player_spawned=True)
+                )
+
+                self.mouse_click_coordinate_pixels = [None, None]
+                self.number_of_shots_taken += 1
+
+    def keyboard_event_handler(self, event):
+        if event.type == pygame.KEYDOWN:
+
+            # If escape is pressed when lmb is held down
+            if event.key == pygame.K_ESCAPE and pygame.mouse.get_pressed()[0] == True:
+                self.mouse_click_coordinate_pixels = [
+                    None, None]  # Cancel action
+                self.show_notification("Input Cancelled")
+
+            elif event.key == pygame.K_COMMA:
+                self.time_mult /= 5
+                self.show_notification(f"x{self.time_mult:.0f} Speed")
+
+            elif event.key == pygame.K_PERIOD:
+                self.time_mult *= 5
+                self.show_notification(f"x{self.time_mult:.0f} Speed")
+
+            elif event.unicode.isdigit():
+                self.selected_planet = int(event.unicode)
+                planet = self.planet_list[self.selected_planet]
+                self.show_notification(
+                    f"{planet.name}: {planet.mass:.0e}Kg")
+
+            elif event.key == pygame.K_r:
+                # restart game
+                self.seconds_passed = 0
+                self.notification_opacity = 0.98
+                self.max_velocity = 0
+                self.number_of_shots_taken = 0
+                self.scenario = 1
+                self.start_scenario()
+
+    def event_handler(self) -> bool:
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return False
+
+            self.mouse_event_handler(event)
+
+            self.keyboard_event_handler(event)
+
+        if not None in self.mouse_click_coordinate_pixels:
+            self.draw_arrow(np.array(self.mouse_click_coordinate_pixels), np.array(
+                pygame.mouse.get_pos()))
+
+        return True
+
+    def draw_scale_bar(self) -> None:
+        """_summary_
+        """
+
+        x1 = 50
+        x2 = x1 + (self.AU * self.AU_PIXELS_CONVERSION)
+
+        y = 50
+        half_arm_width = 25
+
+        pygame.draw.line(self.SCREEN, (255, 255, 255),
+                         (x1, self.WINDOW_HEIGHT-y), (x2, self.WINDOW_HEIGHT-y))
+        pygame.draw.line(self.SCREEN, (255, 255, 255), (x1, self.WINDOW_HEIGHT -
+                         y-half_arm_width), (x1, self.WINDOW_HEIGHT-y+half_arm_width))
+        pygame.draw.line(self.SCREEN, (255, 255, 255), (x2, self.WINDOW_HEIGHT -
+                         y-half_arm_width), (x2, self.WINDOW_HEIGHT-y+half_arm_width))
+        self.TEXT_FONT.render_to(self.SCREEN, ((
+            x2-x1)/2, self.WINDOW_HEIGHT-y-half_arm_width), '1 AU', (250, 250, 250))
+
+        self.seconds_passed += self.delta_t * self.time_mult
+
+        years_passed = self.seconds_passed / (3600*24*365)
+
+        months_passed = 12 * (years_passed % 1)
+
+        years_passed = self.seconds_passed / (3600*24*365)
+
+        self.TEXT_FONT.render_to(self.SCREEN, (
+            x2+50, self.WINDOW_HEIGHT-2*half_arm_width), f"{np.floor(years_passed)} Years {np.floor(months_passed)} Month(s) Passed", (250, 250, 250))
+        self.TEXT_FONT.render_to(self.SCREEN, (
+            x2+50, self.WINDOW_HEIGHT-4*half_arm_width), f"{self.number_of_shots_taken} Shots Taken", (250, 250, 250))
+
+    def show_notification(self, text=None) -> None:
+        x = 10
+        y = 50
+
+        if text:
+            self.notification_text = text
+            self.notification_opacity = 1
+
+        color = np.array((250, 250, 250), dtype=np.int32) * \
+            self.notification_opacity
+        if any(color) < 0:
+            self.notification_opacity = 1
+            self.notification_text = ""
+
+        self.TEXT_FONT.render_to(
+            self.SCREEN, (x, y), self.notification_text, color)
+
+        self.notification_opacity *= self.NOTIFICATION_FADEOUT
+
+    def draw_arrow(self, start_pos, end_pos) -> None:
+
+        new_end_pos = (2*start_pos - end_pos)
+
+        pygame.draw.line(self.SCREEN, (255, 255, 255), start_pos, new_end_pos)
+
+        _, _, vx, vy = self.calculate_slingshot_velocity()
+
+        velocity = f"{round(np.hypot(vx, vy)*1e-3)}Km/s"
+
+        self.TEXT_FONT.render_to(
+            self.SCREEN, new_end_pos + 10, str(velocity), (250, 250, 250))
 
 
 if __name__ == '__main__':
